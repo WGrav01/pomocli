c6301726891faccebf08212888406f29d24d028f
WGrav01 <63617338+WGrav01@users.noreply.github.com>
2024-11-10 18:58:08 -0500

    Created verbose mode and revised help message

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,28 @@ use std::ffi::OsString;
 fn help_message() {
     println!("{}", "Usage: pomocli [options]");
     println!("{}", "Options");
-    println!("{}", "-h, --help: Display this help message");
-    println!("{}", "-f, --focus_time: Set focus time in minutes (default: 25)");
-    println!("{}", "-v, --version: Display version information");
-    println!("{}", "-s, --short_break_time: Set short break time in minutes");
-    println!("{}", "-l, --long_break_time: Set long break time in minutes");
-    println!("{}", "-c, --cycles: Set number of pomodoro cycles until log break");
-    println!("{}", "-a, --attach: Attach to an existing timer");
+    println!("{}", "-h, --help: Display this help message and exit");
+    println!("{}", "-f, --focus_time: Set focus time in HH:MM:SS (default: 25:00)");
+    println!("{}", "-v, --verbose: Enable verbose debug output");
+    println!("{}", "-s, --short_break_time: Set short break time in HH:MM:SS (default: 5:00)");
+    println!("{}", "-l, --long_break_time: Set long break time in HH:MM:SS (default: 10:00)");
+    println!("{}", "-c, --cycles: Set number of pomodoro cycles until log break (default: 4)");
+    println!("{}", "-a, --auto_continue: Move onto the next pomodoro cycle automatically");
+    println!("{}", "-A, --attach: Attach to an existing timer");
 
-    println!("");
+    println!(""); // A newline to separate the options from the version
     println!("{}", "Version [placeholder]")
 }
 
 fn main() {
     let args: Vec<_> = std::env::args_os().collect();
+
+    let mut verbose: bool = false;
+
+    if args.contains(&OsString::from("--verbose")) || args.contains(&OsString::from("-v")) {
+        verbose = true;
+        println!("Arguments received: {:?}", args);
+    }
 
     if args.contains(&OsString::from("--help")) || args.contains(&OsString::from("-h")) {
         help_message();
