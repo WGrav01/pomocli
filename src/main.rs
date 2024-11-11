750472e8d3f4cca0768dbc345ff5d85aad733572
WGrav01 <63617338+WGrav01@users.noreply.github.com>
2024-11-10 20:44:12 -0500

    Remade help message, emptied everything else for now

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
 use std::ffi::OsString;
 
 fn help_message() {
-    println!("{}", "Usage: pomocli [options]");
-    println!("{}", "Options");
-    println!("{}", "-h, --help: Display this help message and exit");
-    println!("{}", "-f, --focus_time: Set focus time in HH:MM:SS (default: 25:00)");
-    println!("{}", "-v, --verbose: Enable verbose debug output");
-    println!("{}", "-s, --short_break_time: Set short break time in HH:MM:SS (default: 5:00)");
-    println!("{}", "-l, --long_break_time: Set long break time in HH:MM:SS (default: 10:00)");
-    println!("{}", "-c, --cycles: Set number of pomodoro cycles until log break (default: 4)");
-    println!("{}", "-a, --auto_continue: Move onto the next pomodoro cycle automatically");
-    println!("{}", "-A, --attach: Attach to an existing timer");
+    let help: &str = "
+    Usage: pomocli [OPTIONS]
 
-    println!(""); // A newline to separate the options from the version
-    println!("{}", "Version [placeholder]")
+    Options:
+        When starting a new timer:
+            -f <HH:MM:SS>, --focus-time <HH:MM:SS>: Time spent in a focus session (default: 25 minutes)
+            -b <HH:MM:SS>, --break-time <HH:MM:SS>: Time spent during a short break (default: 5 minutes)
+            -B <HH:MM:SS>, --break-long-time <HH:MM:SS>: Time spent during a long break (default: 15 minutes)
+            -c <number>, --cycle <number>: Number of cycles before long break (default: 4)
+        Interacting with a running timer:
+            -p, --pause: Pause the timer
+            -r, --resume: Resume the timer
+            -s, --stop: Stop the timer
+            -S, --skip: Skip the current cycle
+            -a, --attach: Attach to a running timer
+        Misc:
+            -h, --help: Show this help message and exit, will ignore all other arguments
+            -v, --verbose: Show detailed debug information
+
+    Version [placeholder]
+    ";
+
+    println!("{}", help);
 }
 
 fn main() {
-    let args: Vec<_> = std::env::args_os().collect();
-
-    let mut verbose: bool = false;
-
-    if args.contains(&OsString::from("--verbose")) || args.contains(&OsString::from("-v")) {
-        verbose = true;
-        println!("Arguments received: {:?}", args);
-    }
-
-    if args.contains(&OsString::from("--help")) || args.contains(&OsString::from("-h")) {
-        help_message();
-        return;
-    }
+    // TODO: Parse command line arguments
 }
